35afe62b6893b02fdfc081b07ffba372283c3bac
Ceyhun Rzayev <rzayev-ceyhun-99@mail.ru>
2023-02-15 00:04:03 +0400

    header3

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -49,10 +49,21 @@ export default function Header() {
                         <div className="nav__right-secondItem">
                             <div className="nav__right-controls">
                                 <div className="nav__language">
-                                    <div className="nav__language-icon"></div>
-                                    <div className="nav__language-text">AZ</div>
+                                    <div className="nav__language-main">
+                                        <div className="nav__language-icon"></div>
+                                        <div className="nav__language-text">AZ</div>
+                                    </div>
+                                    <div className="nav__language-other">
+                                        <div className="nav__language-text">EN</div>
+                                        <div className="nav__language-text">RU</div>
+                                    </div>
                                 </div>
                                 <div className="nav__search"></div>
+                            </div>
+                            <div className="menu-toggle">
+                                <span></span>
+                                <span></span>
+                                <span></span>
                             </div>
                         </div>
                     </div>
