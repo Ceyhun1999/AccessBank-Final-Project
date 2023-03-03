1c914aa35407abae28aa8d17d498bbe4dd4c92ed
Ceyhun Rzayev <rzayev-ceyhun-99@mail.ru>
2023-03-03 21:32:49 +0400

    03-03-2023

--- src/components/headerComponents/menuMobile/MenuMobile.js
+++ src/components/headerComponents/menuMobile/MenuMobile.js
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import { navBarData } from "../../../data/Data";
+import MenuMobileItem from "../menuMobileItem/menuMobileItem";
 import NavRightFirstItem from "../navRightFirstItem/NavRightFirstItem";
 import "./MenuMobile.css";
 
@@ -12,12 +14,9 @@ export default function MenuMobile() {
                 </div>
                 <div className="menu-mobile__content">
                     <ul>
-                        {navBarData.data.map((item) => (
-                            <li>
-                                <p>{item.link}</p>
-                                <span></span>
-                            </li>
-                        ))}
+                        {navBarData.data.map((item, idx) => {
+                            return <MenuMobileItem  key={idx} data={item} idx={idx} />;
+                        })}
                     </ul>
                 </div>
                 <div className="menu-mobile__footer">asagi</div>
---
+++ src/components/headerComponents/menuMobileItem/menuMobileItem.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import "./menuMobileItem.css";
+
+export default function MenuMobileItem({ data, idx }) {
+    const [activeLink, setActiveLink] = useState(100);
+
+    const onChangeActiveLink = (idx) => {
+        if (idx === activeLink) setActiveLink(100);
+        else setActiveLink(idx);
+    };
+
+    useEffect(() => {}, []);
+
+    return (
+        <li key={idx}>
+            <div onClick={() => onChangeActiveLink(idx)} className="mobile-link">
+                <p>{data.link}</p>
+                <span></span>
+            </div>
+            <div
+                className={
+                    activeLink === idx ? "mobile-link__drop mobile-links-active" : "mobile-link__drop"
+                }>
+                <ul>
+                    {data.drop.map((item, idx) => (
+                        <li key={idx}>{item.span}</li>
+                    ))}
+                </ul>
+            </div>
+        </li>
+    );
+}
