7756dacabaed6dd50ec665dfb52b9685791a25e5
Ceyhun Rzayev <rzayev-ceyhun-99@mail.ru>
2023-03-16 14:04:50 +0400

    final

--- src/components/headerComponents/searchMobile/SearchMobile.js
+++ src/components/headerComponents/searchMobile/SearchMobile.js
@@ -1,7 +1,6 @@
 import "./SearchMobile.css";
 
 export default function SearchMobile({ onChangeActiveSearchMobile }) {
-
     return (
         <div onClick={onChangeActiveSearchMobile} className="nav__search-mobile">
             <div className="nav__search-inner-mobile"></div>
--- src/components/mainComponents/main/Main.js
+++ src/components/mainComponents/main/Main.js
@@ -3,7 +3,6 @@ import CurrencyBlock from "../currencyBlock/CurrencyBlock";
 import MyAccess from "../myAccess/MyAccess";
 import MyCard from "../myCard/MyCard";
 import News from "../news/News";
-
 import SectionProducts from "../products/Products";
 import Slider from "../slider/Slider";
 
